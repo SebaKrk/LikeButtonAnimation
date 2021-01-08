81f38c4d25ebc7c76e1550a6ed4cb10be1fabb7d
SebaKrk <s.sciuba@gmail.com>
2021-01-08 14:55:17 +0100

    Initial Commit

---
+++ LikeButtonAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LikeButtonAnimation
+//
+//  Created by Sebastian Sciuba on 08/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
