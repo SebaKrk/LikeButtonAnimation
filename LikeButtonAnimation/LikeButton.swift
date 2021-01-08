5ba20e2f918bfcc5c38a15e234de02418f59b543
SebaKrk <s.sciuba@gmail.com>
2021-01-08 15:17:14 +0100

    create func animate() https://developer.apple.com/documentation/uikit/uiview/1622515-animate https://developer.apple.com/documentation/coregraphics/cgaffinetransform/1455882-scaledby

--- LikeButtonAnimation/LikeButton.swift
+++ LikeButtonAnimation/LikeButton.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LikeButton: UIButton {
     
+    private var isLiked = false
+    
     private let unLikeImage = UIImage(named: "heart_empty")
     private let likeImage = UIImage(named: "heart_filled")
     
@@ -23,5 +25,22 @@ class LikeButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public func  flipLikeState() {
+        isLiked = !isLiked
+        animate()
+    }
+    
+    private func animate() {
+        
+        UIView.animate(withDuration: 0.1, animations: {
+            let newImage = self.isLiked ? self.likeImage : self.unLikeImage
+            self.transform = self.transform.scaledBy(x: 0.1, y: 0.9)
+            self.setImage(newImage, for: .normal)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.transform = CGAffineTransform.identity
+            })
+        })
+    }
 }
 
